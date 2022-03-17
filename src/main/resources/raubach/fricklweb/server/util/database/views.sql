1212c4246454557b22f116b6a85353c1d5a2a976
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2022-03-17 16:00:55 +0000

    - FIX: Fixed null image count for empty albums.

--- src/main/resources/raubach/fricklweb/server/util/database/views.sql
+++ src/main/resources/raubach/fricklweb/server/util/database/views.sql
@@ -9,7 +9,7 @@ CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `album_stats` AS select
     `albums`.`parent_album_id` AS `parent_album_id`,
     (SELECT MAX(`images`.`created_on`) from `images` where `images`.`album_id` = `albums`.`id` and `images`.`data_type` = 'image') AS `newest_image`,
     (SELECT MIN(`images`.`created_on`) from `images` where `images`.`album_id` = `albums`.`id` and `images`.`data_type` = 'image') AS `oldest_image`,
-    (SELECT SUM(`images`.`view_count`) from `images` where `images`.`album_id` = `albums`.`id` and `images`.`data_type` = 'image') AS `image_view_count`,
+    (SELECT COALESCE(SUM(`images`.`view_count`), 0) from `images` where `images`.`album_id` = `albums`.`id` and `images`.`data_type` = 'image') AS `image_view_count`,
     `albums`.`created_on` AS `created_on`,
     `albums`.`updated_on` AS `updated_on`,
     (
